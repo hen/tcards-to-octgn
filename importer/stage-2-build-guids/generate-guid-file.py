67e5a14f5de3fa21a8894d1e67e06f5878d2532b
hen <dev@flamefew.com>
2019-11-07 14:51:09 -0800

    Adding first version of guid creator

---
+++ importer/stage-2-build-guids/generate-guid-file.py
@@ -0,0 +1,39 @@
+import os
+import xml.etree.ElementTree as ET
+from collections import OrderedDict
+import json
+
+# Need to clone the OCTGN_TF repository. 
+# TODO: Make this less hardcoded
+data_location='../OCTGN_TF/f44befce-4d6d-4fb9-a286-9585f36aece9/sets/'
+output_file='target/card_to_guid.json'
+
+card_to_guid = OrderedDict()
+
+for set_directory in os.listdir(data_location):
+    for filename in os.listdir(os.path.join(data_location, set_directory)):
+        if(filename == 'set.xml'):
+            root = ET.parse( os.path.join(data_location, set_directory, filename) ).getroot()
+            octgn_wave = root.attrib['name'].replace('Wave ', '')
+            for card_tag in root.findall('cards/card'):
+                octgn_id = card_tag.attrib['id']
+                octgn_name = card_tag.attrib['name']
+                octgn_cardnum = card_tag.find('./property[@name="Card Number"]').attrib['value']
+                if(octgn_cardnum == ""):
+                    print('Warning: No Card ID for ' + octgn_name + ', using the name')
+                    card_to_guid[octgn_name] = octgn_id
+                    continue
+
+                octgn_cardtype = card_tag.find('./property[@name="Type"]').attrib['value']
+                if(octgn_cardtype == 'Character'):
+                    bot='T'
+                else:
+                    bot=''
+
+                card_id='W{}.{}{}'.format(octgn_wave, bot, octgn_cardnum)
+                card_to_guid[card_id] = octgn_id
+
+output = json.dumps(card_to_guid, indent=4)
+with open(output_file, 'w') as fout:
+    print(output, file=fout)
+fout.close()
